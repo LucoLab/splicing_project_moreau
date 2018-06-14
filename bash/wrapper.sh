901866a4805e90302273f8a8849dcbd85db86306
JP_From_cledebras <jpvillemin@gmail.com>
2018-06-14 23:34:28 +0200

    rechange wrapper

--- bash/wrapper.sh
+++ bash/wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/jean-philippe.villemin/anaconda3/bin/python3 /home/luco/code/python/splicingWhippetPSI.py --config $1 -i /home/jean-philippe.villemin/index_whippet_human.jls  -r /home/jean-philippe.villemin/splicing_project_moreau/Rscript/ -j /home/jean-philippe.villemin/julia-d55cadc350/bin/julia 
+/home/jean-philippe.villemin/anaconda3/bin/python3 /home/jean-philippe.villemin/splicing_project_moreau/src/splicingWhippetPSI.py --config $1 -i /home/jean-philippe.villemin/index_whippet_human.jls  -r /home/jean-philippe.villemin/splicing_project_moreau/Rscript/ -j /home/jean-philippe.villemin/julia-d55cadc350/bin/julia 
